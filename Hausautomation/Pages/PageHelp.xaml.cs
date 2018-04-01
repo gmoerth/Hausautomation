fcfa6c59a8a1bc11c188b79fd93a1056d011fa20
Gerhard Mörth <gmoerth@hotmail.com>
2018-04-02 01:29:17 +0200

    Hilfe Seite und Class Diagramm

--- Hausautomation/Pages/PageHelp.xaml.cs
+++ Hausautomation/Pages/PageHelp.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +16,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web;
 
 // Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
 
@@ -25,6 +30,16 @@ namespace Hausautomation.Pages
         public PageHelp()
         {
             this.InitializeComponent();
+            try
+            {
+                Uri targetUri = new Uri("http://members.chello.at/gmoerth/Grobkonzept_Gerhard_Moerth.html");
+                wvHelp.Navigate(targetUri);
+            }
+            catch (FormatException ex)
+            {
+                Debug.WriteLine(ex.Message.ToString());
+            }
         }
     }
+
 }
